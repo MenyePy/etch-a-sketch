52cb028e015b787946055fcda1b54bd74dac87f4
MenyePy <menyeyt@gmail.com>
2023-11-06 16:51:14 +0200

    Pixel painter v0.02

---
+++ script.js
@@ -0,0 +1,81 @@
+const container = document.querySelector('.container');
+let lengthUnit = 10;
+let containerWidth = 600;
+let containerHeight = 600;
+let mode = "#000";
+let grid = 'border: 1px solid lightgray;';
+
+
+function draw(){
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    let width = containerWidth/lengthUnit;
+    let height  = containerHeight/lengthUnit;
+    for (let i = 1; i <= lengthUnit; i++){
+        let rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+        rowDiv.classList.add(i);
+        container.appendChild(rowDiv);
+
+        for (let j = 0; j < lengthUnit; j++){
+            let colDiv = document.createElement('div');
+            colDiv.classList.add('col');
+            colDiv.classList.add(j);
+            colDiv.setAttribute('style', 'width: ' + width + 'px; height: ' + height + 'px;' + grid);
+            colDiv.addEventListener('mouseenter', (e) => {
+                colDiv.style.backgroundColor = mode;
+            })
+            rowDiv.appendChild(colDiv);
+        }
+    }
+}
+
+const colorBlack = document.querySelector('.black');
+const colorRed = document.querySelector('.red');
+const colorYellow = document.querySelector('.yellow');
+const colorBlue = document.querySelector('.blue');
+const colorGreen = document.querySelector('.green');
+const eraser = document.querySelector('.erase');
+const gridToggle = document.querySelector('.grid');
+
+colorBlack.addEventListener('click', () => {
+    mode = 'black';
+});
+colorRed.addEventListener('click', () => {
+    mode = 'red';
+});
+colorBlue.addEventListener('click', () => {
+    mode = 'blue';
+});
+colorGreen.addEventListener('click', () => {
+    mode = 'green';
+});
+colorYellow.addEventListener('click', () => {
+    mode = 'yellow';
+});
+
+eraser.addEventListener('click', () => {
+    mode = '#fff';
+});
+
+gridToggle.addEventListener('click', () => {
+    grid = (grid == '') ? grid = 'border: 1px solid lightgray;' : grid = '';
+    draw();
+});
+
+function setGrid(){
+    const sliderValue = document.querySelector('#r').value;
+    lengthUnit = sliderValue;
+    draw();
+}
+
+let slider = document.querySelector('#r');
+let output = document.querySelector('.selection');
+output.innerHTML = slider.value;
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+
+draw();
